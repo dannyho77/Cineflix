b791989c2a3d68595bda877dd4f7e33cae59d919
danny <dannyho77@gmail.com>
2021-03-17 17:42:34 -0400

    testing

--- Cineflix/frontend/cineflix.jsx
+++ Cineflix/frontend/cineflix.jsx
@@ -1,15 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-// import Root from './components/root';
-// import configureStore from './store/store';
+import Root from './components/root';
+import configureStore from './store/store';
 
 
-// document.addEventListener('DOMContentLoaded', () => {
-//   const store = configureStore();
-//   const root = document.getElementById('root');
-
-//   ReactDOM.render(<Root store={store} />, root);
-// });
 document.addEventListener('DOMContentLoaded', () => {
-  ReactDOM.render(<div>Cineflix</div>);
+  const store = configureStore();
+  const root = document.getElementById('root');
+
+  ReactDOM.render(<Root store={store} />, root);
 });
