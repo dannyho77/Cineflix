e42590a650bf2d9b16e3ab8384774099da74e481
Danny <dannyho77@gmail.com>
2021-03-20 15:34:23 -0400

    CSS nav bar

--- Cineflix/frontend/components/nav_bar/nav_bar.jsx
+++ Cineflix/frontend/components/nav_bar/nav_bar.jsx
@@ -11,6 +11,8 @@ class NavBar extends React.Component {
                 </div>
 
                 <div className='right-nav'>
+                    <a href="https://github.com/dannyho77"><img src="https://www.freeiconspng.com/thumbs/github-icon/github-icon-9.png" id="github"/></a>
+                    <a href="https://www.linkedin.com/in/danny-ho-a4988360"><img src="https://cdn2.iconfinder.com/data/icons/simple-social-media-shadow/512/14-512.png" id="linkedin"/></a>
                     <Link to="/login" id='login-button'>Sign In</Link>
                 </div>
             </div>
