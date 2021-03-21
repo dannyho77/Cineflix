3df0f964a4d0e544176bfd80f1c81dffbf26cbb7
Danny <dannyho77@gmail.com>
2021-03-21 17:54:01 -0400

    CSS signup page

--- Cineflix/frontend/components/session/signup_form.jsx
+++ Cineflix/frontend/components/session/signup_form.jsx
@@ -26,20 +26,23 @@ class SignupForm extends React.Component {
 
     render(){
         return(
-          <html classname='signup-form-body'>
+          <div classname='signup-form-body'>
             <div id='signup-navbar'><NavBar/></div>
-              <div className="signup-form-container">
+              {/* <div className="signup-form-container"> */}
                   <form onSubmit={this.handleSubmit} className="signup-form-box">
-                      <div id='text1'>Create a password to start your membership.</div>
+                      <div id='text1'>Create a password to start your <br/> membership.</div>
                       <br/>
-                      <input type="text" value={this.state.email} onChange={this.update('email')} id='signup-email' placeholder='Email'/>
-                      <br/>
-                      <input type="password" value={this.state.password} onChange={this.update('password')} id='signup-password' placeholder='Add a password'/>
-                      <br/>
-                      <button className="signup-submit">Continue</button>
+                      <div id='item1'>
+                        <input type="text" value={this.state.email} onChange={this.update('email')} id='signup-email' placeholder='Email'/>
+                        <br/>
+                        <input type="password" value={this.state.password} onChange={this.update('password')} id='signup-password' placeholder='Add a password'/>
+                        <br/>
+                        <button className="signup-submit">Continue</button>
+                      </div>
                   </form>
-              </div>
-          </html>
+              {/* </div> */}
+              <div id='bot-bar'></div>
+          </div>
         );
     }
 }
