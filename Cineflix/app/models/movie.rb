721eca5fc64dc16f829c0b13385fffec9f8b3dfe
Danny <dannyho77@gmail.com>
2021-03-23 21:42:49 -0400

    configuring AWS S3 setup

--- Cineflix/app/models/movie.rb
+++ Cineflix/app/models/movie.rb
@@ -1,2 +1,24 @@
 class Movie < ApplicationRecord
+    validates :title, :description, :year, presence: true
+
+    has_one_attached :photo
+    has_one_attached :video
+
+    has_many :movie_genres,
+    primary_key: :id,
+    foreign_key: :movie_id,
+    class_name: MovieGenre
+
+    has_many :my_lists,
+    primary_key: :id,
+    foreign_key: :movie_id,
+    class_name: MyList
+
+    has_many :genres,
+    through: :movie_genres,
+    source: :genre
+
+    has_many :users,
+    through: :my_lists,
+    source: :user
 end
