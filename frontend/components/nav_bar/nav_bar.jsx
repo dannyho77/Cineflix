225d3d4db0a92277fafff73b416838ace7f6bcc3
Danny <dannyho77@gmail.com>
2021-03-26 10:24:30 -0400

    editing navbar signout button

--- frontend/components/nav_bar/nav_bar.jsx
+++ frontend/components/nav_bar/nav_bar.jsx
@@ -38,25 +38,27 @@ import { Link } from 'react-router-dom';
     //         <Link to="/login" id='login-button'>Sign In</Link>
     //     </div>
     // )
-class NavBar extends React.Component {
 
-        constructor(props){
-            super(props);
-            const logout = this.props.logout;
-        }
+    class NavBar extends React.Component {
 
-        render(){
+            constructor(props){
+                super(props);
+            }
+
+            render(){
+
                 return (
                     <div className='nav'>
                         <img id='logo' src="https://fontmeme.com/permalink/210319/d1842af13f3a1a8770c2a1aabea4152d.png"/>
                         <div className='right-nav'>
                             <a href="https://github.com/dannyho77"><img src="https://www.freeiconspng.com/thumbs/github-icon/github-icon-9.png" id="github"/></a>
                             <a href="https://www.linkedin.com/in/danny-ho-a4988360"><img src="https://cdn2.iconfinder.com/data/icons/simple-social-media-shadow/512/14-512.png" id="linkedin"/></a>
-                            <button onClick={()=>{logout()}} id='logout-button'>Sign Out</button>
+                            <button onClick={() => {this.props.logout().then(() => this.props.history.push('/'));}} id='logout-button'>Sign Out</button>
                         </div>
                     </div>
                 )
-                }
-};
+            }
+                
+    };
 
-export default NavBar;
+    export default NavBar;
