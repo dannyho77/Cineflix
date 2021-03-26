897381310ef59fdb3cd2bac4fb05feb62302205f
Danny <dannyho77@gmail.com>
2021-03-25 20:45:24 -0400

    edit navbar container/pres.component

--- frontend/components/movie/movies_index.jsx
+++ frontend/components/movie/movies_index.jsx
@@ -2,7 +2,8 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 import { SourceMapGenerator } from 'source-map';
 
-import { logout } from '../../actions/session';
+// import { logout } from '../../actions/session';
+import NavBar from '../nav_bar/nav_bar';
 import HomeMovie from './home_movie';
 
 
@@ -11,28 +12,31 @@ class MoviesIndex extends React.Component{
     constructor(props){
         super(props)
         this.state = {movies: []}
+        // this.logout = this.logout.bind(this);
     }
     // componentDidMount(){
     //     this.props.fetchMovies()
     // }
 
-    logout(dispatch){
-        return (user) => dispatch(login(user));
-    }
+    // logout(dispatch){
+    //     return (user) => dispatch(login(user));
+    // }
 
     render(){
 
         return (
         <div className='movies-index'>
 
-            <div className='NavBar'>
+            {/* <div className='NavBar'>
                     <img id='logo' src="https://fontmeme.com/permalink/210319/d1842af13f3a1a8770c2a1aabea4152d.png"/>
                     <div className='right-nav'>
                         <a href="https://github.com/dannyho77"><img src="https://www.freeiconspng.com/thumbs/github-icon/github-icon-9.png" id="github"/></a>
                         <a href="https://www.linkedin.com/in/danny-ho-a4988360"><img src="https://cdn2.iconfinder.com/data/icons/simple-social-media-shadow/512/14-512.png" id="linkedin"/></a>
-                        <button onClick={logout}>Sign Out</button>
+                        <button onClick={this.logout}>Sign Out</button>
                     </div>
-            </div>
+            </div> */}
+
+            <NavBar className='NavBar'/>
 
             <HomeMovie id='main-movie-player'/>
         </div>
--- frontend/components/nav_bar/nav_bar.jsx
+++ frontend/components/nav_bar/nav_bar.jsx
@@ -23,7 +23,7 @@ import { Link } from 'react-router-dom';
 // export default NavBar;
 
 const NavBar = ({currentUser, logout}) => {
-    const display = currentUser ? (
+    const display = !currentUser ? (
         <div>
             <Link to='/' id='logo'><img src="https://fontmeme.com/permalink/210319/d1842af13f3a1a8770c2a1aabea4152d.png"/></Link>
             <a href="https://github.com/dannyho77"><img src="https://www.freeiconspng.com/thumbs/github-icon/github-icon-9.png" id="github"/></a>
@@ -41,7 +41,12 @@ const NavBar = ({currentUser, logout}) => {
 
     return (
         <div className='nav'>
-            {display}
+            <img id='logo' src="https://fontmeme.com/permalink/210319/d1842af13f3a1a8770c2a1aabea4152d.png"/>
+            <div className='right-nav'>
+                <a href="https://github.com/dannyho77"><img src="https://www.freeiconspng.com/thumbs/github-icon/github-icon-9.png" id="github"/></a>
+                <a href="https://www.linkedin.com/in/danny-ho-a4988360"><img src="https://cdn2.iconfinder.com/data/icons/simple-social-media-shadow/512/14-512.png" id="linkedin"/></a>
+                <button onClick={logout} id='logout button'>Sign Out</button>
+            </div>
         </div>
     )
 };
