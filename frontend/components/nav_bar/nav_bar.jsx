fcd19e58914d6da9a7120b1d513ebba7134f21c7
Danny <dannyho77@gmail.com>
2021-03-26 10:53:04 -0400

    navbar signout button code

--- frontend/components/nav_bar/nav_bar.jsx
+++ frontend/components/nav_bar/nav_bar.jsx
@@ -39,26 +39,16 @@ import { Link } from 'react-router-dom';
     //     </div>
     // )
 
-    class NavBar extends React.Component {
-
-            constructor(props){
-                super(props);
-            }
-
-            render(){
-
+    export default ({logout}) => {
                 return (
                     <div className='nav'>
                         <img id='logo' src="https://fontmeme.com/permalink/210319/d1842af13f3a1a8770c2a1aabea4152d.png"/>
                         <div className='right-nav'>
                             <a href="https://github.com/dannyho77"><img src="https://www.freeiconspng.com/thumbs/github-icon/github-icon-9.png" id="github"/></a>
                             <a href="https://www.linkedin.com/in/danny-ho-a4988360"><img src="https://cdn2.iconfinder.com/data/icons/simple-social-media-shadow/512/14-512.png" id="linkedin"/></a>
-                            <button onClick={() => {this.props.logout().then(() => this.props.history.push('/'));}} id='logout-button'>Sign Out</button>
+                            <button onClick={logout} id='logout-button'>Sign Out</button>
                         </div>
                     </div>
                 )
-            }
                 
     };
-
-    export default NavBar;
