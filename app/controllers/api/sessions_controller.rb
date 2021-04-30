bbdab3f68c86995871cc3057e1cfa72648631744
Danny <dannyho77@gmail.com>
2021-04-30 15:08:32 -0400

    revise controllers and other cleanups

--- app/controllers/api/sessions_controller.rb
+++ app/controllers/api/sessions_controller.rb
@@ -5,9 +5,9 @@ class Api::SessionsController < ApplicationController
 
         if @user
             login(@user)
-            render "/api/users/show"
+            render "api/users/show"
         else
-            render json: ['Invalid username or password'], status: 401
+            render json: ['Invalid email or password.'], status: 401
         end
     end
 
