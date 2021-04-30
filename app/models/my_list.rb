9701b32ed5f7a0308338f6e4a22c40e75ab6faac
Danny <dannyho77@gmail.com>
2021-04-30 12:06:44 -0400

    rails db models

--- app/models/my_list.rb
+++ app/models/my_list.rb
@@ -1,2 +1,13 @@
 class MyList < ApplicationRecord
+    validates :user_id, :movie_id, presence: true
+    
+    belongs_to :user
+    primary_key: :id,
+    foreign_key: :user_id,
+    class_name: :User
+
+    belongs_to :movie
+    primary_key: :id,
+    foreign_key: :movie_id,
+    class_name: :Movie
 end
