3b23ae970908b549e1887307453a9550a7a06d63
Danny <dannyho77@gmail.com>
2021-05-04 16:47:31 -0400

    list controller

---
+++ app/controllers/api/mylists_controller.rb
@@ -0,0 +1,30 @@
+class Api::MylistsController < ApplicationController
+
+    def show
+        render :show
+    end
+
+    def index
+        @mylists = MyList.all
+    end
+
+    def create
+        @mylist = MyList.new(user_id: current_user.id, movie_id: params[:movie_id])
+        if @mylist.save
+            render json: {movie_id: @mylist[:movie_id]}
+        else
+            render json: {message: "Failed to add title to List"}, status: 422
+        end
+    end
+
+    def destroy
+        @mylist = current_user.my_lists.find_by(movie_id: params[:movie_id])
+        if @mylist
+            @mylist.destroy
+            render :show
+        else
+            render json: {message: "This title does not exist in your List"}, status: 422
+        end
+    end
+
+end
