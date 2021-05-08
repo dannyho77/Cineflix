8e80cacd9805508c54e145cf3bcece7077381af8
Danny <dannyho77@gmail.com>
2021-05-08 16:26:43 -0400

    reducers

--- frontend/reducers/errors_reducer.js
+++ frontend/reducers/errors_reducer.js
@@ -1,5 +1,21 @@
-const errors = () => {
+import {
+    RECEIVE_CURRENT_USER,
+    RECEIVE_ERRORS,
+    CLEAR_ERRORS
+} from '../actions/session'
 
+const errors = (state = [], action) => {
+    Object.freeze(state);
+    switch (action.type) {
+        case RECEIVE_ERRORS:
+            return action.errors;
+        case RECEIVE_CURRENT_USER:
+            return [];
+        case CLEAR_ERRORS:
+            return [];
+        default:
+            return state;
+    }
 };
   
 export default errors;
