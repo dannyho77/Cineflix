b794c9368ed16a47726394572f5b3e4408e5eeb0
Danny <dannyho77@gmail.com>
2021-05-17 18:32:42 -0400

    genre/single movie item component

--- frontend/components/genre/genre_item.jsx
+++ frontend/components/genre/genre_item.jsx
@@ -4,7 +4,84 @@ import { Link } from 'react-router-dom';
 class GenreItem extends React.Component {
     constructor(props) {
       super(props);
-      
+      this.state = {preview: null, muted: true};
+      this.handleSound = this.handleSound.bind(this);
+      this.handleAddRemove = this.handleAddRemove.bind(this);
     }
 
+    handleSound(){
+        e.preventDefault();
+        this.setState({muted:!this.state.muted});
+    }
+
+    handleAddRemove(){
+        const {
+            mylists,
+            movie,
+            addToMyList,
+            removeFromMyList,
+            currentUser
+        } = this.props;
+
+        const mylist = mylists.find(mylist => {
+            mylist.movie_id === movie.id
+        });
+
+        if (mylist) {
+            removeFromMyList(mylist.id);
+        } else {
+            addToMyList({
+                user_id: currentUser.id,
+                movie_id: movie.id
+            });
+        }
+    }
+
+    render(){
+        const {movie, mylists, previewing} = this.props;
+        const mylist = mylists.find(mylist => {
+            mylist.movie_id === movie.id
+        });
+        const thumbnail = <img src={movie.photoURL} />;
+        const previewing = this.state.preview === movie.id;
+
+        const sound = muted ? (
+            <button className="sound-button" onClick={this.handleSound}>
+                <i className="fas fa-volume-mute"></i>
+            </button>
+            ) : (
+            <button className="sound-button" onClick={this.handleSound}>
+                <i className="fas fa-volume-up"></i>
+            </button>
+        );
+
+        const listbutton = (
+            <button>
+                
+            </button>
+        )
+
+        const preview = (
+            <div className = "single-movie">
+                <Link to = {`/movies/${movie.id}`}>
+                    <video className = "single-movie-video" autoPlay>
+                        <source src = {movie.movieURL} type="video/mp4"/>
+                        {sound}
+                    </video>
+                </Link>
+                <div className ="single-movie-controls">
+
+                </div>
+                <div className ="single-movie-info">
+
+                </div>
+            </div>
+        );
+
+        return(
+            <div className = "genre-movie" onMouseEnter = {() => this.setState({ preview: movie.id })} OnMouseLeave = {() => this.setState({ preview: null })}>
+                {previewing ? preview : thumbnail}
+            </div>
+        );
+    }
 }
